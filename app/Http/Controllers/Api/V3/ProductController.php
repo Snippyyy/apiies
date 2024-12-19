9ad1c48ca8b016e9ad2bdd1a4930191231bc70fb
Snippyyy <Snippy_06@hotmail.com>
2024-12-19 16:49:55 +0100

    Ejercicio 7
    CRUD productos, se ha incluido en el grupo del middleware para que solo sea accesible si se esta autenticado

--- app/Http/Controllers/Api/V3/ProductController.php
+++ app/Http/Controllers/Api/V3/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\V3;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 
@@ -13,5 +14,32 @@ class ProductController extends Controller
         $products = Product::with('category')->paginate(9);
 
         return ProductResource::collection($products);
+    }
+    public function show(Product $product)
+    {
+        return new ProductResource($product);
+    }
+
+    public function store(ProductRequest $request)
+    {
+        $product = Product::create($request->validated());
+
+        return new ProductResource($product);
+    }
+
+    public function update(ProductRequest $request, Product $product)
+    {
+        $product->update($request->validated());
+
+        return new ProductResource($product);
+    }
+
+    public function destroy(Product $product)
+    {
+        $product->subcategory()->detach();
+
+        $product->delete();
+
+        return response()->json(['message' => 'Se ha borrado el producto'], 204);
     }
 }
