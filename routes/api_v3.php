d3619e0afd90af3cf0a277488769148ecc6b7cda
Snippyyy <Snippy_06@hotmail.com>
2024-12-19 15:26:31 +0100

    Ejercicio 1
    Cambio a la V3

---
+++ routes/api_v3.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\Api\V3\CategoryController;
+use App\Http\Controllers\Api\V3\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('lists/categories', [CategoryController::class,  'list']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('categories', CategoryController::class);
+
+    Route::get('products', [ProductController::class,  'index'])
+        ->middleware('throttle:products');
+});
